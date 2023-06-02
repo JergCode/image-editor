6073afb94dfad4e7719ea502d196e5d9d0421fab
JE Ramirez <emmanuel.ramirezg@yahoo.com>
2023-06-02 12:51:17 -0700

    Add movement to pic when zoomed in

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,4 @@
-import {
-  Component,
-  ViewChild,
-  ElementRef,
-  AfterViewInit,
-  OnDestroy,
-} from '@angular/core';
-import { Subscription, fromEvent } from 'rxjs';
+import { Component, ViewChild, ElementRef, AfterViewInit } from '@angular/core';
 
 const GAP = 50;
 @Component({
@@ -13,47 +6,72 @@ const GAP = 50;
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css'],
 })
-export class AppComponent implements AfterViewInit, OnDestroy {
-  viewer: any;
+export class AppComponent implements AfterViewInit {
   imageSrc = '/assets/img/pic1.png';
   brightness: number = 1;
   contrast: number = 1;
   zoom: number = 1;
   left: number = 0;
   top: number = 0;
-  weelSubscription?: Subscription;
+  imageBouding: DOMRect;
 
-  @ViewChild('image', { static: false }) image?: ElementRef<HTMLImageElement>;
-
-  ngAfterViewInit(): void {
-    this.weelSubscription = fromEvent(window, 'wheel').subscribe(
-      (ev: Event) => {
-        if (this.isWheelEvent(ev)) {
-          let wheelEvent = ev as WheelEvent;
-          const newzoom = this.zoom - wheelEvent.deltaY * 0.001;
-
-          const y = wheelEvent.clientY;
-          const x = wheelEvent.clientX;
-          if (this.isWithinImageBoundary(x, y))
-            if (newzoom < 6) {
-              this.zoom = Math.max(newzoom, 1);
-              this.top = this.getTopPosition(y);
-              this.left = this.getLeftPosition(x);
-            }
-        }
-      }
-    );
+  constructor(image: ElementRef<HTMLImageElement>) {
+    this.image = image;
+    this.imageBouding = image.nativeElement.getBoundingClientRect();
   }
 
-  isWithinImageBoundary(x: number, y: number): boolean {
-    const domRect = this.image?.nativeElement.getBoundingClientRect();
-    if (domRect == null) return false;
+  @ViewChild('image', { static: false }) image: ElementRef<HTMLImageElement>;
 
+  ngAfterViewInit(): void {
+    this.image.nativeElement.ondragstart = () => false;
+    this.imageBouding = this.image.nativeElement.getBoundingClientRect();
+  }
+
+  onMouseInImage(event: MouseEvent) {
+    document.addEventListener('wheel', this.onWheelScroll);
+  }
+
+  onWheelScroll = (event: WheelEvent) => {
+    const newzoom = this.zoom - event.deltaY * 0.001;
+    const y = event.clientY;
+    const x = event.clientX;
+    if (this.isWithinImageBoundary(x, y))
+      if (newzoom < 6) {
+        this.zoom = Math.max(newzoom, 1);
+        this.top = this.getTopPosition(y);
+        this.left = this.getLeftPosition(x);
+      }
+  };
+
+  onMouseOutImage(event: MouseEvent) {
+    document.removeEventListener('wheel', this.onWheelScroll);
+    document.removeEventListener('mousemove', this.onMouseMove);
+  }
+
+  onMousedown(event: MouseEvent) {
+    if (this.zoom > 1) {
+      document.addEventListener('mousemove', this.onMouseMove);
+
+      this.image.nativeElement.onmouseup = (event: MouseEvent) => {
+        document.removeEventListener('mousemove', this.onMouseMove);
+        this.image.nativeElement.onmouseup = null;
+      };
+    }
+  }
+
+  onMouseMove = (event: MouseEvent) => {
+    console.log('x', event.x - this.imageBouding.left);
+    console.log('y', event.y - this.imageBouding.top);
+    this.top = this.getTopPosition(event.y - this.imageBouding.top);
+    this.left = this.getLeftPosition(event.x - this.imageBouding.left);
+  };
+
+  isWithinImageBoundary(x: number, y: number): boolean {
     return (
-      x > domRect.left + GAP &&
-      x < domRect.right - GAP &&
-      y > domRect.top + GAP &&
-      y < domRect.bottom - GAP
+      x > this.imageBouding.left + GAP &&
+      x < this.imageBouding.right - GAP &&
+      y > this.imageBouding.top + GAP &&
+      y < this.imageBouding.bottom - GAP
     );
   }
 
@@ -67,39 +85,31 @@ export class AppComponent implements AfterViewInit, OnDestroy {
   }
 
   getLeftPosition(x: number): number {
-    const domRect = this.image?.nativeElement.getBoundingClientRect();
     const left =
-      domRect == null
+      this.imageBouding == null
         ? x
-        : x > domRect.right
+        : x > this.imageBouding.right
         ? x - this.zoom / 2
         : x - this.zoom / 2 - 2 * GAP;
 
     return Math.min(left, x);
   }
 
+  getPicTopPosition(y: number) {}
+
   getTopPosition(y: number) {
-    const domRect = this.image?.nativeElement.getBoundingClientRect();
     const top =
-      domRect == null
+      this.imageBouding == null
         ? y
-        : y > domRect.bottom
+        : y > this.imageBouding.bottom
         ? y - this.zoom / 2
         : y - this.zoom / 2 - 5 * GAP;
 
     return Math.min(top, y);
   }
 
-  ngOnDestroy(): void {
-    this.weelSubscription?.unsubscribe();
-  }
-
   getValue(value: number): string {
     return `${Math.floor(value * 100)}%`;
-  }
-
-  isWheelEvent(ev: Event): Boolean {
-    return ev instanceof WheelEvent;
   }
 
   getStyle() {
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,21 +5,21 @@ import { AppComponent } from './app.component';
 
 import { NgStyle } from '@angular/common';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import {MatSliderModule} from '@angular/material/slider';
-import {MatToolbarModule} from '@angular/material/toolbar';
+import { MatSliderModule } from '@angular/material/slider';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { CdkDrag } from '@angular/cdk/drag-drop';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
+  declarations: [AppComponent],
   imports: [
     BrowserModule,
     BrowserAnimationsModule,
     MatSliderModule,
     MatToolbarModule,
+    CdkDrag,
     NgStyle,
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
